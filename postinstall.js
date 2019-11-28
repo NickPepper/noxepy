c3d00642846f2e27e7d7879b7bf495b119c9b825
NickPershin <nickpeppper@gmail.com>
2019-11-28 12:30:12 +0300

    quick fix

--- postinstall.js
+++ postinstall.js
@@ -1,19 +1,18 @@
-const path         = require('path')
-    , varstring    = require('varstring')
-    , getos        = require('getos')
-    , fs           = require('fs')
-    , after        = require('after')
-    , instructions = require('./exercises/am_i_ready/vars.json').instructions
+const path         = require('path'),
+      varstring    = require('varstring'),
+      getos        = require('getos'),
+      fs           = require('fs'),
+      instructions = require('./exercises/foo/vars.json').instructions
 
 
-const tmpl    = path.join(__dirname, 'exercises/am_i_ready/problem.md.tmpl')
-    , out     = path.join(__dirname, 'exercises/am_i_ready/problem.md')
-    , problem = fs.readFileSync(tmpl, 'utf-8')
+const out     = path.join(__dirname, 'exercises/foo/problem.md'),
+      problem = fs.readFileSync(out, 'utf-8')
 
 
 getos(function (err, os) {
-  if (err)
+  if (err) {
     throw err
+  }
 
   var lookup = os.dist ? os.dist : os.os
   var markdown = varstring(problem, instructions[lookup] || instructions.Other)
